80c646a29727fa41869552021414a4f6342927dd
brnthngs <bpwetteroff@gmail.com>
2017-04-11 18:23:28 -0500

    Commit

---
+++ Chapter 6.17/Chapter 6.17/ch6_17.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_17
+	{
+	class ch6_17
+		{
+		/*6.17 (Calculating Sales) A website sells three products whose retail prices are as follows: product
+1, $2.98; product 2, $4.50; and product 3, $9.98.Write an app that reads a series of pairs of numbers
+as follows:
+			a) product number
+			b) quantity sold
+Your app should use a switch statement to determine the retail price for each product. It should
+calculate and display the total retail value of all products sold. Use a sentinel-controlled loop to
+determine when the app should stop looping and display the final results.*/
+		static void Main ( string [ ] args )
+			{
+			double product_1 = 2.98;
+			double product_2 = 4.50;
+			double product_3 = 9.98;
+			int quantity;
+			double counter = 0;
+
+
+			while (counter != -99)
+				{
+				switch (  )
+				}
+
+
+			}
+		}
+	}
